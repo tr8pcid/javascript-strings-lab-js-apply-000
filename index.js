fad27c83ff91e8649937e02d8aced5916a7d19bf
Tassin islam <itztassin@gmail.com>
2020-04-29 06:04:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
-var conversation = `Lets talk about ${topic}.`
+var conversation = `Lets talk about ${topic}.`;
